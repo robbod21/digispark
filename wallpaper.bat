5ab9db95b4ede57690dfce9b049d61c303e51b6e
robbod21 <46109559+robbod21@users.noreply.github.com>
2018-12-24 17:45:12 +0100

    Add files via upload

---
+++ wallpaper.bat
@@ -0,0 +1,16 @@
+:: Download Wallpaper
+
+cd %homepath%\AppData\Local\Microsoft\Windows\Themes\RoamedThemeFiles\DesktopBackground
+
+powershell -Command "Invoke-WebRequest https://upload.wikimedia.org/wikipedia/de/thumb/4/4e/Sea_Shepherd_Logo.svg/2000px-Sea_Shepherd_Logo.svg.png -UseBasicParsing -OutFile 2000px-Sea_Shepherd_Logo.svg.png"
+
+
+
+:: Set Wallpaper
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%cd%\2000px-Sea_Shepherd_Logo.svg.png" /f
+
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 0 /f
+
+
+pause
